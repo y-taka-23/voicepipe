90fbbcf2f88fadb1a5e8e5afe4dab9fa3f908b40
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-05-20 03:06:32 +0900

    Implement simple lexing

--- unmarshal.go
+++ unmarshal.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"strings"
+)
+
+func LogicalLines(src string) []string {
+	s := strings.Replace(src, "\\\n", " ", -1)
+	return strings.Split(s, "\n")
+}
+
+func Tokenize(line string) []string {
+	s := strings.SplitN(line, "#", 1)[0]
+	return strings.Fields(s)
+}
+
 func (df *Dockerfile) Unmarshal(src []byte) error {
 	return nil
 }
