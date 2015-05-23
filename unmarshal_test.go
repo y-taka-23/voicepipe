ab30b9640bf308b322654248f67979727b744722
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-05-23 23:04:39 +0900

    Add tests for USER parsing

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -26,3 +26,25 @@ func TestParseMaintainer(t *testing.T) {
 		}
 	}
 }
+
+func TestParseUser(t *testing.T) {
+	cases := []struct {
+		in   []byte
+		want *User
+	}{
+		{[]byte("root"), &User{Name: "root"}},
+		{[]byte(" root"), &User{Name: "root"}},
+		{[]byte("root "), &User{Name: "root"}},
+	}
+	for _, c := range cases {
+		got, _ := ParseUser(c.in)
+		if got.Name != c.want.Name {
+			t.Errorf(
+				"ParseUser(%q).Name == %q, want %q",
+				c.in,
+				got.Name,
+				c.want.Name,
+			)
+		}
+	}
+}
