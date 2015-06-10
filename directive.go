2486c262c5a65a61bccc609affa67f09d7d1e001
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-06-10 23:28:03 +0900

    Split files

---
+++ directive.go
@@ -0,0 +1,42 @@
+package main
+
+type ImageDirective struct {
+	Tag        string
+	Parameters map[string]string
+}
+
+type Directive struct {
+	Repository      string
+	ImageDirectives []*ImageDirective
+}
+
+type intermediateDirective struct {
+	Repository string
+	Images     []struct {
+		Tag        string
+		Parameters []struct {
+			Name  string
+			Value string
+		}
+	}
+}
+
+func (d *Directive) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var in = &intermediateDirective{}
+	if err := unmarshal(in); err != nil {
+		return err
+	}
+	d.Repository = in.Repository
+	d.ImageDirectives = make([]*ImageDirective, 0)
+	for _, i := range in.Images {
+		params := make(map[string]string)
+		for _, p := range i.Parameters {
+			params[p.Name] = p.Value
+		}
+		d.ImageDirectives = append(
+			d.ImageDirectives,
+			&ImageDirective{Tag: i.Tag, Parameters: params},
+		)
+	}
+	return nil
+}
--- voicepipe.go
+++ voicepipe.go
@@ -11,48 +11,6 @@ import (
 	"os/exec"
 )
 
-type ImageDirective struct {
-	Tag        string
-	Parameters map[string]string
-}
-
-type Directive struct {
-	Repository      string
-	ImageDirectives []*ImageDirective
-}
-
-type intermediateDirective struct {
-	Repository string
-	Images     []struct {
-		Tag        string
-		Parameters []struct {
-			Name  string
-			Value string
-		}
-	}
-}
-
-func (d *Directive) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var in = &intermediateDirective{}
-	if err := unmarshal(in); err != nil {
-		return err
-	}
-	d.Repository = in.Repository
-	d.ImageDirectives = make([]*ImageDirective, 0)
-	for _, i := range in.Images {
-		params := make(map[string]string)
-		for _, p := range i.Parameters {
-			params[p.Name] = p.Value
-		}
-		d.ImageDirectives = append(
-			d.ImageDirectives,
-			&ImageDirective{Tag: i.Tag, Parameters: params},
-		)
-	}
-
-	return nil
-}
-
 func Resources(root string) ([]os.FileInfo, error) {
 	rs := make([]os.FileInfo, 0)
 	fis, err := ioutil.ReadDir(root)
