6e04d47d08dbeefc560e9418d2c3fc768a2d1a7d
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-06-14 18:16:06 +0900

    Fix the condition not to ignore Dockerfiles

--- voicepipe.go
+++ voicepipe.go
@@ -37,7 +37,7 @@ func (vp *VoicePipe) Resources() ([]os.FileInfo, error) {
 	}
 	for _, fi := range fis {
 		n := fi.Name()
-		if n != ".voicepipe" && n != "voicepipe.yml" && n != "Dockerfile" {
+		if n != ".voicepipe" && n != "voicepipe.yml" {
 			rs = append(rs, fi)
 		}
 	}
@@ -53,15 +53,15 @@ func (vp *VoicePipe) Setup(id ImageDirective, rs []os.FileInfo) error {
 		return err
 	}
 	for _, fi := range rs {
+		src := path.Join(vp.RootDir, fi.Name())
 		if fi.Name() != "Dockerfile" {
-			src := path.Join(vp.RootDir, fi.Name())
 			tgt := path.Join(dir, fi.Name())
 			if err := os.Link(src, tgt); err != nil {
 				return err
 			}
 			continue
 		}
-		buf, err := ioutil.ReadFile(path.Join(vp.RootDir, fi.Name()))
+		buf, err := ioutil.ReadFile(src)
 		if err != nil {
 			return err
 		}
@@ -73,7 +73,8 @@ func (vp *VoicePipe) Setup(id ImageDirective, rs []os.FileInfo) error {
 		for k, v := range id.Parameters {
 			df = ReplaceEnv(*df, k, v)
 		}
-		if err := ioutil.WriteFile(fi.Name(), df.Marshal(), 775); err != nil {
+		tgt := path.Join(dir, fi.Name())
+		if err := ioutil.WriteFile(tgt, df.Marshal(), 775); err != nil {
 			return err
 		}
 	}
